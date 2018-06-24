ebbac3503b7e32928f9ec09ba56c40ee8c3de5d0
Toad <Toad@localhost>
2018-06-23 21:19:22 -0700

    Added Inventory Added Public Sprites Added Player

---
+++ Devilguard/Actor_Type.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
+using System;
+
+namespace Devilguard
+{
+    class Actor_Type
+    {
+        public Point Location;
+        public Rectangle Hitbox;
+        public float Speed;
+        public Inventory inventory = new Inventory();
+        public bool UsedItem;
+
+    }
+}
---
+++ Devilguard/Inventory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devilguard
+{
+
+    enum SD_UI:int
+    {
+        Back,
+        Button,
+        Resources
+    };
+
+
+    enum Item
+    {
+        Wood,
+        Stone,
+        Iron,
+        Dirt
+    };
+
+
+
+   
+    class Inventory
+    {
+        public Dictionary<Item, int> Items = new Dictionary<Item, int>();
+
+
+        public void AddItem(Item i, int amount)
+        {
+            if (Items.ContainsKey(i))
+                Items[i] += amount;
+            else
+                Items.Add(i, amount);
+        }
+
+        public void RemoveItem(Item i, int amount)
+        {
+            if (Items.ContainsKey(i))
+            {
+                Items[i] -= amount;
+                if (Items[i] <= 0)
+                    Items.Remove(i);
+            }            
+        }
+
+        public int ItemCount(Item i)
+        {
+            if (Items.ContainsKey(i))
+                return Items[i];
+            else
+                return 0;
+
+            
+        }
+
+        
+
+
+    }
+}
